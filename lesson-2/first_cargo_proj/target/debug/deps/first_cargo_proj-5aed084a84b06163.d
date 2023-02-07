d849ad63e7a2422c24f0ef478ef77d4ae4849618
codeTIT4N <lokeshkumar636@yahoo.com>
2023-02-07 21:25:42 +0530

    small fix

---
+++ lesson-2/first_cargo_proj/target/debug/deps/first_cargo_proj-5aed084a84b06163.d
@@ -0,0 +1,5 @@
+/Users/lokeshkumar/dev/rust/rust-school/lesson-2/first_cargo_proj/target/debug/deps/first_cargo_proj-5aed084a84b06163.rmeta: src/main.rs
+
+/Users/lokeshkumar/dev/rust/rust-school/lesson-2/first_cargo_proj/target/debug/deps/first_cargo_proj-5aed084a84b06163.d: src/main.rs
+
+src/main.rs:
