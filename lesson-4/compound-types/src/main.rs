76bf5416ec66c700bd196cffd986846033a6cca7
codeTIT4N <lokeshkumar636@yahoo.com>
2023-02-13 21:33:15 +0530

    array type

--- lesson-4/compound-types/src/main.rs
+++ lesson-4/compound-types/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // Tuple type
     let tup1 = (450, 6.2, 1);
-    println!("{:?}", tup1);
+    println!("Tuple: {:?}", tup1);
 
     // Tuple type with type annotations
     let tup2: (i32, f64, u8) = (450, 6.2, 1);
@@ -19,4 +19,31 @@ fn main() {
     println!("The value of a is: {a}");
     println!("The value of b is: {b}");
     println!("The value of c is: {c}");
+
+    // Array Type
+    let arr = [1, 2, 3, 4, 5];
+    println!("Number array: {:?}", arr);
+
+    let days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
+    println!("Days: {:?}", days);
+
+    let array: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Type annotated array: {:?}", array);
+
+    // Accessing array elements
+    let new_array = [1, 2, 3, 4, 5];
+    let fist_element = new_array[0];
+    let second_element = new_array[1];
+
+    println!("Array: {:?}", new_array);
+    println!("fist element is: {fist_element}");
+    println!("second element is: {second_element}");
 }
